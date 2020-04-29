2bac12fdb7753d5e0e25005b8e499d64b8c4b0d6
Michal Yelkin <michalkin9@gmail.com>
2020-04-30 01:02:11 +0300

    Update Rotator.cs

--- matala_basic/Assets/scripts/Rotator.cs
+++ matala_basic/Assets/scripts/Rotator.cs
@@ -9,9 +9,7 @@ public class Rotator : MonoBehaviour
     // Start is called before the first frame update
 
     void Start()
-    {
-        
-    }
+    {}
 
     // Update is called once per frame
     void Update()
